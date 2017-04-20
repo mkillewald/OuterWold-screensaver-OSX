942637f014a90640972190434331fe6ca3c19e3c
Mike Killewald <mkillewald@yahoo.com>
2017-04-20 11:29:10 -0400

    Initial Commit

---
+++ OuterWorld/OuterWorldView.h
@@ -0,0 +1,13 @@
+//
+//  OuterWorldView.h
+//  OuterWorld
+//
+//  Created by k1ds3ns4t10n on 4/20/17.
+//  Copyright © 2017 Gameaholix. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface OuterWorldView : ScreenSaverView
+
+@end
---
+++ OuterWorld/OuterWorldView.m
@@ -0,0 +1,52 @@
+//
+//  OuterWorldView.m
+//  OuterWorld
+//
+//  Created by k1ds3ns4t10n on 4/20/17.
+//  Copyright © 2017 Gameaholix. All rights reserved.
+//
+
+#import "OuterWorldView.h"
+
+@implementation OuterWorldView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
